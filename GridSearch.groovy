a3e50e70c35b846d6783e068c6d3b8f6f344f23d
Matt Tu <matttu120@gmail.com>
2015-11-18 11:58:09 -0800

    Update GridSearch.groovy

--- GridSearch.groovy
+++ GridSearch.groovy
@@ -29,25 +29,6 @@ class GridSearch {
 	 * @param num the number to find
 	 * @return the position of the number or null
 	 */
-	/*
-	 * My first thought for this solution was to start from i=0 and j=0, and search every element somehow (whether by
-	 * binary search or one by one). However, we come across a problem when we reach an element around the middle of 
-	 * the grid. Consider the following: 
-	 * 
-	 *  1	2	3	4	5
-	 *  2	3	6	8	7
-	 *  3	5	10	11	12
-	 *  4	7	11	12	15
-	 *  4	8	12	15	18
-	 *  
-	 *  Say we wanted to do a search for 10. We'd have to throw out the binary search option because we have no way
-	 *  of knowing which direction to search unless we search them all. This is fine and would take n(logn) time, but
-	 *  we want to optimize it so that we don't have to recurse upon the entire grid.
-	 *  
-	 *  Instead, what if we search from the bottom left of the grid, at i=0 and j=n? We effectively only search a 
-	 *  diagonal from the bottom left of the grid to where the element should be. This would take linear time in all cases,
-	 *  and thankfully for me is much simpler to implement!
-	 */
 
 	Position findInGrid(grid, int num) {
 		int x = 0;
